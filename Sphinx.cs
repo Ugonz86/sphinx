6deceabfca666106265b955ee0da8ae9170fd465
Na Hyung Choi and Uriel Gonzalez <na@>
2019-07-15 10:51:41 -0700

    This will commit the recent additions

--- Sphinx.cs
+++ Sphinx.cs
@@ -1,20 +1,28 @@
 using System;
+using System.Collections.Generic;
 
 class Sphinx
 {
+
+  public static Dictionary<int, string> questions = new Dictionary<int, string>() { {0, "No sooner spoken than broken?"}, {1, "What does Mill + Walk + Key equals?"}, {2, "What gets wetter and wetter the more it dries?"}, {3, "I have a tail, and I have a head, but I have no body. I am NOT a snake. What am I?"} };
+
+  public static Dictionary<int, string> answers = new Dictionary<int, string>() { {0, "silence"}, {1, "Milwaukee"}, {2, "Towel"}, {3, "A coin"} };
+
   static void Main()
   {
-    Console.WriteLine("No sooner spoken than broken?");
-    string answer = Console.ReadLine();
+    Random rand = new Random();
+    int index = rand.Next(0, 4);
 
-    if (answer == "silence")
+    Console.WriteLine(questions[index]);
+    string input = Console.ReadLine();
+
+    if (input == answers[index])
     {
-      Console.WriteLine("The sphinx has been defeated!");
+      Console.WriteLine("The sphinx have been defeated!");
     }
     else
     {
-      Console.WriteLine("You have been defeated!");
+      Console.WriteLine("You have been defeated");
     }
   }
-
 }
